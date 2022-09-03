10c533eee0aa2d30eee281939a568ed945b136dc
Gregory John Casamento <greg.casamento@gmail.com>
2022-09-03 06:24:09 -0400

    Initial Commit

---
+++ NSTableCellView_test/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  NSTableCellView_test
+//
+//  Created by Gregory Casamento on 9/3/22.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ NSTableCellView_test/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  NSTableCellView_test
+//
+//  Created by Gregory Casamento on 9/3/22.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+    }
+    return NSApplicationMain(argc, argv);
+}
